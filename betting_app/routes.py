d5373cd8e95b9425b26a31ff6fe80556a5f6b49b
bugrahaskan <bugrahaskan@outlook.com>
2024-06-16 14:48:00 +0000

    webhook trying

--- betting_app/routes.py
+++ betting_app/routes.py
@@ -33,3 +33,15 @@ def test():
     print('ok')
     return 'test'
 
+@app.route('/set_webhook', methods=['GET', 'POST'])
+async def async_set_webhook():
+    s = await BOT.setWebhook(url='{URL}/test_webhook'.format(URL=URL))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/test_webhook', methods=['GET','POST'])
+def test():
+
+    return 'test webhook'
