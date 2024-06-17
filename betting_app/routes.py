f66c62bda1ebedf8646f042ed182e63f2b471e20
bugrahaskan <bugrahaskan@outlook.com>
2024-06-17 09:57:32 +0000

    polling routes

--- betting_app/routes.py
+++ betting_app/routes.py
@@ -12,6 +12,7 @@ import telegram
 from telegram import BotCommand, Update, Bot
 from telegram.ext import Application, CommandHandler, ContextTypes, Updater
 from queue import Queue
+import threading
 
 from .utils import read_config, extract_args
 from .models import Database
@@ -43,17 +44,29 @@ def help_command(update: Update, context: ContextTypes):
 def echo(update: Update, context: ContextTypes):
     update.message.reply_text(update.message.text)
 
-# Updater from Telegram API
-update_queue = Queue()
-#updater = Updater(bot=TOKEN, update_queue=update_queue)
-application = Application.builder().token(TOKEN).update_queue(update_queue).build()
+async def set_commands(application):
+    commands = [
+        BotCommand("start", "Start the bot"),
+        BotCommand("help", "Get help"),
+        BotCommand("echo", "Echo me")
+    ]
+    
+    await application.bot.set_my_commands(commands)
 
-async def trigger_bot():
+async def main():
+    # Updater from Telegram API
+    update_queue = Queue()
+    #updater = Updater(bot=TOKEN, update_queue=update_queue)
+    application = Application.builder().token(TOKEN).update_queue(update_queue).build()
+
     # Add handlers for different commands and messages
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("echo", echo))
 
+    # Set bot commands
+    await set_commands(application)
+    
     # Start the Bot with polling
     await application.initialize()
     await application.start()
@@ -61,17 +74,44 @@ async def trigger_bot():
     await application.stop()
     await application.shutdown()
 
-commands = [
-    BotCommand("start", "Start the bot"),
-    BotCommand("help", "Get help"),
-    BotCommand("echo", "Echo me")
-]
+    return application
 
-async def set_commands(commands):
-    await application.bot.set_my_commands(commands)
 
-loop_set_command = asyncio.new_event_loop()
-asyncio.set_event_loop(loop_set_command)
-loop_set_command.run_until_complete(set_commands(commands))
+#def run_telegram_bot():
+#    asyncio.run(main())
 
-asyncio.run(trigger_bot())
+#bot_thread = threading.Thread(target=run_telegram_bot)
+#bot_thread.start()
+
+application = asyncio.run(main())
+
+@app.route('/')
+def index():
+    print('ok')
+    return 'test'
+
+@app.route('/test', methods=['GET'])
+def test():
+    print('ok')
+    return 'test'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        update = Update.de_json(request.get_json(force=True), application.bot)
+        #application.update_queue.put(update)
+        asyncio.create_task(application.update.update_queue.put(update))
+        return 'ok'
+
+async def async_set_webhook():
+    await application.bot.setWebhook(url='{URL}/webhook'.format(URL=URL))
+
+@app.route('/set_webhook', methods=['GET','POST'])
+def set_webhook():
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(async_set_webhook())
+        return "Webhook set successfully!"
+    except Exception as e:
+        return f"Failed to set webhook: {e}"
