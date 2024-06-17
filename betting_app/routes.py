f809e101bb998e74354dc5c5bb74e25e3783817c
bugrahaskan <bugrahaskan@outlook.com>
2024-06-17 06:09:08 +0000

    sync method for help_command

--- betting_app/routes.py
+++ betting_app/routes.py
@@ -49,13 +49,13 @@ async def send_message(chat_id, msg_id, msg):
 #    #print('help_command')
 #    #await asyncio.sleep(1)
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = context.args[0]
     print(msg)
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
 
-    await BOT.send_message(chat_id=update.effective_chat.id, text='These are the available commands:\n/start - Start the bot\n/help - Get help')
+    BOT.send_message(chat_id=update.effective_chat.id, text='These are the available commands:\n/start - Start the bot\n/help - Get help')
     #await context.bot.send_message(chat_id=update.effective_chat.id, text='These are the available commands:\n/start - Start the bot\n/help - Get help')
     #await BOT.sendMessage(chat_id=chat_id, text=msg, reply_to_message_id=msg_id)
 
